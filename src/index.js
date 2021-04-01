d56f1225f23e88b8bef11603c66993b59c541441
Herbert Paes <herbert_pbarreto@hotmail.com>
2021-04-01 10:56:35 -0300

    add put, delete

--- src/index.js
+++ src/index.js
@@ -1,8 +1,68 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
 const app = express();
 
-app.get('/', (request, response) => {
-  response.send({ message: 'Hello World!' });
+app.use(express.json());
+
+/**
+ * Query Params: Vamos usar principalmente para filtros e paginação
+ * Route Params: Identificar recursos na hora de atualizar ou deletar
+ * Request Params: Resto do conteúdo na hora de criar ou editar um recurso
+ */
+
+const projects = [];
+
+app.get('/projects', (request, response) => {
+  response.json(projects);
+});
+
+app.post('/projects', (request, response) => {
+  const { title, owner } = request.body;
+
+  const project = {
+    id: uuidv4(),
+    title,
+    owner,
+  };
+  projects.push(project);
+
+  response.status(200).json(project);
+});
+
+app.put('/projects/:id', (request, response) => {
+  const { id } = request.params;
+  const { title, owner } = request.body;
+
+  const projectIndex = projects.findIndex((project) => project.id === id);
+
+  if (!projectIndex) {
+    return response.status(400).json({ error: 'Project not found' });
+  }
+
+  const project = {
+    id,
+    title,
+    owner,
+  };
+
+  projects[projectIndex] = project;
+
+  return response.json(project);
+});
+
+app.delete('/projects/:id', (request, response) => {
+  const { id } = request.params;
+  console.log(id);
+  const projectIndex = projects.findIndex((project) => project.id == id);
+
+  console.log(projectIndex);
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: 'Project not found' });
+  }
+
+  projects.splice(projectIndex, 1);
+
+  return response.status(204).send();
 });
 
 module.exports = app;
